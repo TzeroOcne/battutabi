4adf5ec72932322cc6d6ad1ea7992247598f2a94
Tzero Ocne <tzeroocne.interface@gmail.com>
2025-11-30 21:45:30 +0700

    feat: add daisyui

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import electronLogo from './assets/electron.svg'
-
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 </script>
 
 <img alt="logo" class="logo" src={electronLogo} />
@@ -14,10 +12,7 @@
 </div>
 <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
 <div class="actions">
-  <div class="action">
-    <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a></div>
-  <div class="action">
-    <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions a11y-missing-attribute-->
-    <a target="_blank" class="bg-red-700!" rel="noreferrer" on:click={ipcHandle}>Send IPC</a>
-  </div>
+  <button class="btn btn-primary">
+    Click Me
+  </button>
 </div>
