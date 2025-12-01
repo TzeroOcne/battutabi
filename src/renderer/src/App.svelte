8a88f042855f98fc4c2db96a28f9f8ae0be311bb
Tzero Ocne <tzeroocne.interface@gmail.com>
2025-12-01 16:23:34 +0700

    feat(tab): add tabbed address bar

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-  let url = $state('https://electron-vite.org');
+  import { randomUUID } from "./util"
+
+  let urlList = $state<[string, string][]>([
+    [randomUUID(), 'https://electron-vite.org'],
+    [randomUUID(), 'https://google.com'],
+  ]);
 
   function loadBase(newUrl: string): void {
     console.log('Loading URL:', newUrl);
@@ -7,9 +12,28 @@
   }
 
   const load = (): void => loadBase(url);
+
+  let url = $state(urlList[0][1]);
+  let tabIdx = $state(0);
+
+  $inspect({ url });
 </script>
 
-<div class="h-8 flex *:h-full gap-2 mx-2">
-  <input type="text" class="input w-full" bind:value={url}>
-  <button class="btn btn-primary" onclick={load}>Load</button>
+<div class="h-12 *:h-full [&>div]:border-base-300 gap-2">
+  <div class="h-6 w-full tabs tabs-lift">
+    {#each urlList as item, idx (item[0])}
+      <input
+        bind:group={tabIdx}
+        type="radio"
+        name="browser-tab"
+        class="tab"
+        aria-label={item[1]}
+        value={idx}
+      >
+    {/each}
+    <div class="tab"></div>
+  </div>
+  <div class="h-6 w-full">
+    <input type="text" class="input w-full" bind:value={urlList[tabIdx][1]}>
+  </div>
 </div>
