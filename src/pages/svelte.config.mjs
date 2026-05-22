63bab7f56b10f25d4e5d1b3f291f87117b77b5af
Tzero Ocne <tzeroocne.interface@gmail.com>
2026-05-23 01:59:47 +0700

    chore(config): update svelte.config.mjs

--- src/pages/svelte.config.mjs
+++ src/pages/svelte.config.mjs
@@ -1,5 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
